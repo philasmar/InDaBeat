274aece01d2d9e25b0a6aa18be95906a7dd10dce
Phil Asmar <phil.asmar@gmail.com>
2019-10-04 17:17:20 -0400

    Added Oculus stuff and initial scene

---
+++ Assets/CubeActions.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeActions : MonoBehaviour
+{
+    public GameObject music;
+    AudioSource audio;
+    // Start is called before the first frame update
+    void Start()
+    {
+        audio = music.GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonUp(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+                Interact(hit);
+        }
+    }
+
+    void Interact(RaycastHit hit)
+    {
+        if (hit.collider.name.Equals("Cube"))
+        {
+            if (audio.isPlaying)
+            {
+                audio.Pause();
+            }
+            else
+            {
+                audio.Play();
+            }
+        }
+    }
+}
